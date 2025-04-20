6fbc33965028ea2e85828870908cd4b101a4f55e
DRAKE <drakem32@gmail.com>
2025-04-19 19:24:25 -0500

    Final Stage First Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,27 @@
 const editProfileBtn = document.querySelector('.profile__edit-button');
 const editProfileModal = document.querySelector('#edit-profile-modal');
 const editProfileCloseBtn = editProfileModal.querySelector('.modal__close-btn');
+const editProfileForm = editProfileModal.querySelector('.modal__form');
+const editProfileNameInput = editProfileModal.querySelector('#profile-name-input');
+const editProfileDescriptionInput = editProfileModal.querySelector('#profile-description-input');
 
 const newPostBtn = document.querySelector('.profile__add-button');
 const newPostModal = document.querySelector('#new-post-modal');
 const newPostCloseBtn = newPostModal.querySelector('.modal__close-btn');
 
+const newPostForm = newPostModal.querySelector('.modal-form');
+const cardImageInput = newPostModal.querySelector('#card-image-input');
+const cardDescriptionInput = newPostModal.querySelector('#caption-description-input');
+
+const profileNameEl = document.querySelector('.profile__name');
+const profileDescriptionEl = document.querySelector('.profile__description');
+
+
+
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -23,3 +37,20 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal__is-opened");
+}
+
+editProfileForm.addEventListener('submit', handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(cardImageInput, cardDescriptionInput);
+  newPostModal.classList.remove("modal__is-opened");
+}
+
+newPostForm.addEventListener('submit', handleAddCardSubmit);
